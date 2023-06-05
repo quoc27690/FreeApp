dfa5650cd14fde6f8007d492c1584502075c3ec2
Lam Anh Quoc <anhquoc@evol.vn>
2023-06-05 15:02:46 +0700

    FlatListAnimations

--- src/navigators/Main.tsx
+++ src/navigators/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Example } from '../screens';
 import { createStackNavigator } from '@react-navigation/stack';
+import FlatListAnimations from '../screens/FlatListAnimations';
 
 const Stack = createStackNavigator();
 
@@ -8,7 +9,7 @@ const Stack = createStackNavigator();
 const MainNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Example} />
+      <Stack.Screen name="Home" component={FlatListAnimations} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/FlatListAnimations/components/Rating.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+export type Props = {
+  rating: number;
+};
+
+const Rating: React.FC<Props> = ({ rating }) => {
+  const filledStars = Math.floor(rating / 2);
+  const maxStars = Array(5 - filledStars).fill('staro');
+  const r = [...Array(filledStars).fill('star'), ...maxStars];
+
+  return (
+    <View style={styles.rating}>
+      <Text style={styles.ratingNumber}>{rating}</Text>
+      {r.map((type, index) => {
+        return <Icon key={index} name={type} size={12} color="tomato" />;
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ratingNumber: { marginRight: 4, fontFamily: 'Menlo', fontSize: 14 },
+  rating: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 4,
+  },
+});
+
+export default Rating;
